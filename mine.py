e3f9776f778eca4cd0596fbc677f568691d7b8f6
Rimcoin <38360606+rimcoin@users.noreply.github.com>
2018-05-09 13:37:35 -0400

    Updated mine.py accordingly.

--- mine.py
+++ mine.py
@@ -1,16 +1,11 @@
 from Crypto.Hash import SHA256
 import os,random,sys,subprocess
 args=sys.argv
-hashes=1
-diff=2**240
-for j in range(int(hashes/1024)):
-    diff=diff*(131071/131072)
+diff=float(subprocess.check_output(("curl "+args[1]+"/get_diff -s").split(" ")))
+os.system("curl "+args[1]+"/new_mine -s")
 while True:
     r=str(hex(random.randint(1,(2**32)))[2:])
     Hash=SHA256.new()
     Hash.update(r)
     if int(Hash.hexdigest(),16)<(diff):
-        print("w00t")
-        sys.stdout.flush()
         os.system("curl "+sys.argv[1]+"/submit*"+r+"*"+sys.argv[2])
-        hashes+=1
