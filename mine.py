5db435c940f5cbd311693b6c9fbd0862c859687e
Rimcoin <38360606+rimcoin@users.noreply.github.com>
2018-05-09 08:29:55 -0400

    UPDATE mine.py
    Next, we will update the rq_node.py.

--- mine.py
+++ mine.py
@@ -1,15 +1,16 @@
 from Crypto.Hash import SHA256
 import os,random,sys,subprocess
 args=sys.argv
-os.system("curl "+args[1]+"/get_hash -O -s")
-hashes=int(open("get_hash","r").read())
+hashes=1
 diff=2**240
 for j in range(int(hashes/1024)):
     diff=diff*(131071/131072)
 while True:
-    r=str(hex(random.randint(1,(2**4096)))[2:])
+    r=str(hex(random.randint(1,(2**32)))[2:])
     Hash=SHA256.new()
     Hash.update(r)
     if int(Hash.hexdigest(),16)<(diff):
+        print("w00t")
+        sys.stdout.flush()
         os.system("curl "+sys.argv[1]+"/submit*"+r+"*"+sys.argv[2])
         hashes+=1
